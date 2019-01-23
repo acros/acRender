707fed3f5b8ba229e8818f328556721b59beaeab
acros <zhao_zh88@outlook.com>
2019-01-23 13:45:03 +0800

    Add SDL & SDL_image

---
+++ acRender/include/utils.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <utility>
+#include <SDL.h>
+
+
+/*
+ * Get the resource path for resources located in res/subDir
+ * It's assumed the project directory is structured like:
+ * bin/
+ *  the executable
+ * res/
+ *  Lesson1/
+ *  Lesson2/
+ *
+ * Paths returned will be Project_Root/res/subDir
+ */
+std::string getResourcePath(const std::string &subDir = "") {
+#ifdef _WIN32
+	const char PATH_SEP = '\\';
+#else
+	const char PATH_SEP = '/';
+#endif
+	static std::string baseRes;
+	if (baseRes.empty()) {
+		char *basePath = SDL_GetBasePath();
+		if (basePath) {
+			baseRes = basePath;
+			SDL_free(basePath);
+		}
+		else {
+			std::cerr << "Error getting resource path: " << SDL_GetError() << std::endl;
+			return "";
+		}
+		//We replace the last bin/ with res/ to get the the resource path
+		size_t pos = baseRes.rfind("bin");
+		baseRes = baseRes.substr(0, pos) + "res" + PATH_SEP;
+	}
+	return subDir.empty() ? baseRes : baseRes + subDir + PATH_SEP;
+}
+
+
+/*
+ * Recurse through the list of arguments to clean up, cleaning up
+ * the first one in the list each iteration.
+ */
+template<typename T, typename... Args>
+void cleanup(T *t, Args&&... args) {
+	//Cleanup the first item in the list
+	cleanup(t);
+	//Clean up the remaining arguments
+	cleanup(std::forward<Args>(args)...);
+}
+/*
+ * These specializations serve to free the passed argument and also provide the
+ * base cases for the recursive call above, eg. when args is only a single
+ * item one of the specializations below will be called by
+ * cleanup(std::forward<Args>(args)...), ending the recursion
+ */
+template<>
+inline void cleanup<SDL_Window>(SDL_Window *win) {
+	if (!win) {
+		return;
+	}
+	SDL_DestroyWindow(win);
+}
+template<>
+inline void cleanup<SDL_Renderer>(SDL_Renderer *ren) {
+	if (!ren) {
+		return;
+	}
+	SDL_DestroyRenderer(ren);
+}
+template<>
+inline void cleanup<SDL_Texture>(SDL_Texture *tex) {
+	if (!tex) {
+		return;
+	}
+	SDL_DestroyTexture(tex);
+}
+template<>
+inline void cleanup<SDL_Surface>(SDL_Surface *surf) {
+	if (!surf) {
+		return;
+	}
+	SDL_FreeSurface(surf);
+}
+
