ef230a04c26c737ed485e3ab0e7c36872be28f7e
acros <zhao_zh88@outlook.com>
2019-01-24 18:35:39 +0800

    Fix render

--- acRender/src/Scene/TextureScene.cpp
+++ acRender/src/Scene/TextureScene.cpp
@@ -1,6 +1,7 @@
 #include "TextureScene.h"
 #include "Base/AcUtils.h"
 
+GLushort rect_indices[] = { 0, 1, 2, 0, 2, 3 };
 
 
 TextureScene::TextureScene(Renderer& renderer)
@@ -93,24 +94,26 @@ void TextureScene::enter()
 			1.0f,  0.0f         // TexCoord 3
 	};
 
-	 GLushort rect_indices[] = { 0, 1, 2, 0, 2, 3 };
+
+
+
 
 	glGenBuffers(1, &mPosId);
 	glBindBuffer(GL_ARRAY_BUFFER, mPosId);
-//	glBufferData(GL_ARRAY_BUFFER, 3*  sizeof(vVertices), vVertices, GL_STATIC_DRAW);
-	glBufferData(GL_ARRAY_BUFFER, 3 *  sizeof(GLfloat) * (5), vVertices, GL_STATIC_DRAW);
+	int sz = sizeof(vVertices);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vVertices), vVertices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), NULL);// (GLfloat*)vVertices);
 	glEnableVertexAttribArray(0);//For Vertex
 
-
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));// offset);
 	glEnableVertexAttribArray(1);//For UV
 
+	/*
 	glGenBuffers(1, &mElemIdx);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mElemIdx);
-//	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(rect_indices), rect_indices, GL_STATIC_DRAW);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(GLushort), rect_indices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(rect_indices), rect_indices, GL_STATIC_DRAW);
+	*/
 
 	GLenum sx = glGetError();
 	if (sx != GL_NO_ERROR)
@@ -122,39 +125,29 @@ void TextureScene::render()
 {
 	mRendererRef.beginDraw();
 
-//	glBindBuffer(GL_ARRAY_BUFFER, 0);
-//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-	// Use the program object
-	glUseProgram(mShaderProgram);
-
 	// Bind the texture
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, mTexId);
 
- 	glBindVertexArray(mVaoId);
-	glBindBuffer(GL_ARRAY_BUFFER, mPosId);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mElemIdx);
+	glUseProgram(mShaderProgram);
 
-	// Load the vertex position
-//	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), vVertices);
-	// Load the texture coordinate
-//	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), &vVertices[3]);
+//  glBindVertexArray(mVaoId);
+// 	glBindBuffer(GL_ARRAY_BUFFER, mPosId);
+// 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mElemIdx);
 
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
 
 //	glUniform1i(mSampleLocation, 0);
-//	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, rect_indices);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, rect_indices);
 
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+//	glDrawArrays(GL_TRIANGLES, 0, 6);
 
 	GLenum sx = glGetError();
 	if (sx != GL_NO_ERROR)
 		logMessage("GL state bind vertex VBO: %d \n", sx);
 
 	glBindTexture(GL_TEXTURE_2D, 0);
-
 
 	glDisableVertexAttribArray(0);
 	glDisableVertexAttribArray(1);
